9a5f65a1cb248f6ffe0fc62c68c21e803b0d90c8
Kim-bk <nguyenhoangkim120201@gmail.com>
2022-11-05 10:16:17 +0700

    kim - add cors to https://fatalmistake-hub.github.io/

--- CommercialClothes/Program.cs
+++ CommercialClothes/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
                       {
                           builder.WithOrigins("http://localhost:3000"
                               , "https://www.2clothy.tk", "https://2clothy.tk"
-                              , "https://sellercenter.2clothy.tk", "https://www.sellercenter.2clothy.tk")
+                              , "https://fatalmistake-hub.github.io")
                           .AllowAnyHeader()
                           .AllowAnyMethod();
                       });
