7be5f3f7f7cec6e523fdf09cf963813ef345cc48
Kim-bk <nguyenhoangkim120201@gmail.com>
2022-11-28 18:21:36 +0700

    kim - finish payment

--- CommercialClothes/Migrations/20221128032844_Update-Properties.Designer.cs
+++ CommercialClothes/Migrations/20221128032844_Update-Properties.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CommercialClothes.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CommercialClothes.Migrations
 {
     [DbContext(typeof(ECommerceSellingClothesContext))]
-    partial class ECommerceSellingClothesContextModelSnapshot : ModelSnapshot
+    [Migration("20221128032844_Update-Properties")]
+    partial class UpdateProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CommercialClothes/Models/Entities/Account.cs
+++ CommercialClothes/Models/Entities/Account.cs
@@ -19,6 +19,8 @@ namespace CommercialClothes.Models
         public string Password { get; set; }
         public string? Name { get; set; }
         public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
         public string? PhoneNumber { get; set; }
         public DateTime DateCreated { get; set; }
         public int? UserGroupId { get; set; }
