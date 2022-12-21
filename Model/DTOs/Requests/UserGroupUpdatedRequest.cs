c43897ddf7f4762832af6a4b36ee7e64e838e006
Kim-bk <nguyenhoangkim120201@gmail.com>
2022-12-21 20:17:43 +0700

    kim - delete model

--- Model/DTOs/Requests/UserGroupUpdatedRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Model.DTOs.Requests
-{
-    public class UserGroupUpdatedRequest
-    {
-        public int UserId { get; set; }
-        public int UserGroupId { get; set; }
-    }
-}
--- Model/DTOs/Responses/RefreshTokenResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommercialClothes.Models.DTOs.Responses.Base;
-
-namespace CommercialClothes.Models.DTOs.Responses
-{
-    public class RefreshTokenResponse : GeneralResponse
-    {
-        public RefreshToken RefreshToken { get; set; }
-    }
-}
